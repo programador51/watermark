86da7d2194b7a921f474abfeb49576b3cc7c31f4
programador51 <gskitcat@gmail.com>
2022-07-02 18:25:59 -0500

    fix: Types

--- pages/api/auth/types.d.ts
+++ pages/api/auth/types.d.ts
@@ -1,4 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextAuthOptions } from "next-auth";
 
-export type NextAuthMiddleware = (req: NextApiRequest, res: NextApiResponse) => (req: NextApiRequest, res: NextApiResponseoptions: NextAuthOptions);
+// export type NextAuthMiddleware = (req: NextApiRequest, res: NextApiResponse) => (req: NextApiRequest, res: NextApiResponse);
+export type NextAuthMiddleware = (req: NextApiRequest, res: NextApiResponse) => {
+    req: NextApiRequest, res: NextApiResponse
+}
